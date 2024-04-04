9de8a868117916bdd9d7d59a8b8f752816d3a1b1
Kinshoop <82682248+Kinshoop@users.noreply.github.com>
2024-04-04 22:03:33 +0200

    Add files via upload

---
+++ gestionnaire.c
@@ -0,0 +1,103 @@
+#include <signal.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include "tache.h"
+
+#define PIPE "fifo"
+
+int pid;
+struct sigaction act_gestionnaire, act_pere, act_tache;
+int fd;
+
+void fonct_gestionnaire() {
+    //fin du processus gestionnaire de tache
+    exit(0);
+}
+
+void fonct_pere() {
+    printf("Fin du gestionnaire de tache\n");
+    fflush(stdout);
+
+    kill(pid, SIGUSR1); // Envoie un signal au processus enfant (gestionnaire) pour qu'il arrête la tâche
+    close(fd);
+    exit(0);
+}
+
+void fonct_tache() {
+   // Fin d'une tache qui est trop longue
+    exit(0);
+}
+
+void exec_tache(tache* tache) {
+    pid_t tache_pid;
+
+    tache_pid = fork();
+    if (tache_pid < 0) {
+        perror("Erreur lors de la création du processus pour la tâche");
+        fflush(stdout);
+        exit(EXIT_FAILURE);
+    }
+    else if (tache_pid == 0) {
+        pid_t timer_pid;
+        timer_pid = fork();
+        if (timer_pid == 0) {
+            // Timer qui vérifie qu'une tache ne dépasse pas 5 secondes
+            int t = 0;
+            while (t < 5) {
+                sleep(1);
+                t++;
+            }
+            if (kill(getppid(), SIGUSR2) == 0) { //kill renvoi 0 si le processus existe encore (délai dépassé)
+                printf("Fin de la tache %d car execution >5s \n", tache->id);
+                fflush(stdout);
+            }
+            exit(0);
+        }
+        else if (timer_pid > 0) {
+            // Handler qui gère l'interruption d'une tache trop longue
+            act_tache.sa_handler = fonct_tache;
+            sigaction(SIGUSR2, &act_tache, 0);
+            printf("Executeur de tâche : ID %d, Temps d'exécution : %ds\n", tache->id, tache->duree_exec);
+            fflush(stdout);
+            // Simulation d'une execution de tache
+            sleep(tache->duree_exec);
+            printf("Executeur de tâche : Tâche %d terminée\n", tache->id);
+            fflush(stdout);
+            exit(0);
+        }
+    }
+}
+
+int main() {
+    pid = fork();
+    if (pid == 0) {
+       // Mise en place du gestionnaire de tache
+        act_gestionnaire.sa_handler = fonct_gestionnaire;
+        sigaction(SIGUSR1, &act_gestionnaire, 0);
+        mknod(PIPE, S_IFIFO | 0660, 0); // création du pipe
+        fd = open(PIPE, O_RDONLY, 0); // ouvrir le pipe
+        tache readtache = {-1,-1};
+        while (1) {
+            if (read(fd, &readtache, sizeof(tache)) < 0) { // lecture du pipe
+                perror("Error reading pipe");
+                exit(1);
+            }
+            if (readtache.duree_exec != -1) { //durée = -1 si aucune nouvelle tache n'est lu
+                exec_tache(&readtache);
+                readtache.duree_exec = -1;
+            }
+        }
+    }
+    if (pid > 0) {
+        act_pere.sa_handler = fonct_pere;
+        sigaction(SIGINT, &act_pere, 0);
+        while (1) {
+            pause();
+        }
+    }
+}
